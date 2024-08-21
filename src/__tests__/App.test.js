a56b7b712c26be01c631d3626efa4636362082a9
Jaewon Kim <126300912+jkim906@users.noreply.github.com>
2024-08-21 16:36:46 +1200

    fix:change import

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,4 @@
-import { act, React } from "react";
+import { act } from "react";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import App from "../App";
