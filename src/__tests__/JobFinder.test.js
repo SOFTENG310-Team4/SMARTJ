8c73996043c6aecabceb074552cd470983a91d82
axu732 <axu732@aucklanduni.ac.nz>
2024-10-08 04:38:34 +1300

    feat: added more tests for the server endpoints

--- src/__tests__/JobFinder.test.js
+++ src/__tests__/JobFinder.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import JobFinder from "../pages/JobFinder"; // Adjust the import path if necessary
-
+import JobFinder from "../pages/JobFinder";
 describe("JobFinder component", () => {
   beforeEach(() => {
     render(<JobFinder />);
